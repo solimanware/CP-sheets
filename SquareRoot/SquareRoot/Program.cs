c30d072c9e60feaacf84de7bde46a24d6af0b539
Osama Soliman <eng.microsmsm@gmail.com>
2019-11-03 16:07:46 +0200

    init

---
+++ SquareRoot/SquareRoot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SquareRoot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a, b, c, d, x1, x2;
+			Console.Write("Enter a: ");
+			a = double.Parse(Console.ReadLine());
+			Console.Write("Enter b: ");
+			b = double.Parse(Console.ReadLine());
+			Console.Write("Enter c: ");
+			c = double.Parse(Console.ReadLine());
+			d = b * b - 4 * a * c;
+			if (a == 0)
+			{
+				//straight line
+				Console.WriteLine("It's straight line equation");
+			}
+			else
+			{
+				//quadratic equation
+				if (d >= 0)
+				{
+					x1 = (-b + Math.Sqrt(d)) / 2 * a;
+					x2 = (-b - Math.Sqrt(d)) / 2 * a;
+					Console.WriteLine("x1 = {0} and x2 = {1}", x1, x2);
+				}
+				else
+				{
+					Console.WriteLine("No Real Solution");
+				}
+			}
+
+		}
+	}
+}
---
+++ StudentGrade/StudentGrade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StudentGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double subject1, subject2, subject3, subject4, subject5, studentGrade, totalGrade;
+            totalGrade = 500;
+            Console.Write("Subject1: ");
+            subject1 = double.Parse(Console.ReadLine());
+            Console.Write("Subject2: ");
+            subject2 = double.Parse(Console.ReadLine());
+            Console.Write("Subject3: ");
+            subject3 = double.Parse(Console.ReadLine());
+            Console.Write("Subject4: ");
+            subject4 = double.Parse(Console.ReadLine());
+            Console.Write("Subject5: ");
+            subject5 = double.Parse(Console.ReadLine());
+            studentGrade = (subject1 + subject2 + subject3 + subject4 + subject5) / totalGrade;
+            Console.WriteLine("Total Grade = {0}", studentGrade);
+
+
+
+        }
+    }
+}
